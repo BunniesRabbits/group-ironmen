6b4d10076085ad809868337e31f3a666a8a687d8
BunniesRabbits <212995513+BunniesRabbits@users.noreply.github.com>
2025-06-12 00:26:51 -0700

    better logging for vite-express to catch errors

--- site/scripts/server.ts
+++ site/scripts/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import ViteExpress from "vite-express";
 import winston from "winston";
 import expressWinston from "express-winston";
+import pc from "picocolors";
 
 const app = express();
 
@@ -22,8 +23,19 @@ const mode: "production" | "development" = productionMode ? "production" : "deve
 
 const port = 4000;
 
+const timestamp = new Date().toLocaleString("en-US").split(",")[1].trim();
+console.log(
+  `${pc.dim(timestamp)} ${pc.bold(pc.blue("[server.ts]"))} Attempting to run vite-express on port ${port}...`,
+);
+
 ViteExpress.config({ mode });
-ViteExpress.listen(app, port, () => {
-  console.log(`Server is listening on '${port}'...`);
-});
-console.log("Listening stopped, exiting...");
+ViteExpress.listen(app, port)
+  .on("error", (err: { code: string }) => {
+    const msg = `Failed to listen: ${err.code ?? "Unknown Reason"}`;
+    const timestamp = new Date().toLocaleString("en-US").split(",")[1].trim();
+    console.log(`${pc.dim(timestamp)} ${pc.bold(pc.blue("[server.ts]"))} ${pc.red(msg)}`);
+  })
+  .on("listening", () => {
+    const timestamp = new Date().toLocaleString("en-US").split(",")[1].trim();
+    console.log(`${pc.dim(timestamp)} ${pc.bold(pc.blue("[server.ts]"))} Success! Listening on port ${port}...`);
+  });
