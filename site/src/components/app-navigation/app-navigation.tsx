75f05bb775862b378516737db70e1423bfde977a
BunniesRabbits <212995513+BunniesRabbits@users.noreply.github.com>
2025-06-16 20:20:25 -0700

    navigation buttons highlight when on that page

--- site/src/components/app-navigation/app-navigation.tsx
+++ site/src/components/app-navigation/app-navigation.tsx
@@ -2,21 +2,26 @@ import type { ReactElement } from "react";
 import { MenLink } from "../men-link/men-link";
 
 import "./app-navigation.css";
+import { useLocation } from "react-router-dom";
 
 export const AppNavigation = ({ groupName }: { groupName: string }): ReactElement => {
+  const location = useLocation();
+
+  const links = [
+    { label: "Items", href: "/group/items" },
+    { label: "Map", href: "/group/map" },
+    { label: "Graphs", href: "/group/graphs" },
+    { label: "Panels", href: "/group/panels" },
+    { label: "Settings", href: "/group/settings" },
+    { label: "Setup", href: "/setup-instructions" },
+    { label: "Logout", href: "/logout" },
+    { label: "Homepage", href: "/" },
+  ].map(({ label, href }) => <MenLink key={label} label={label} href={href} selected={location.pathname === href} />);
+
   return (
     <div id="app-navigation" className="rsborder-tiny rsbackground">
       <h4 className="app-navigation__group-name">{groupName}</h4>
-      <nav className="app-navigation__nav">
-        <MenLink label="Items" href="/group/items" />
-        <MenLink label="Map" href="/group/map" />
-        <MenLink label="Graphs" href="/group/graphs" />
-        <MenLink label="Panels" href="/group/panels" />
-        <MenLink label="Settings" href="/group/settings" />
-        <MenLink label="Setup" href="/setup-instructions" />
-        <MenLink label="Logout" href="/logout" />
-        <MenLink label="Homepage" href="/" />
-      </nav>
+      <nav className="app-navigation__nav">{links}</nav>
     </div>
   );
 };
--- site/src/components/men-link/men-link.tsx
+++ site/src/components/men-link/men-link.tsx
@@ -5,10 +5,11 @@ import { Link } from "react-router";
 export interface MenLinkProps {
   label: string;
   href: string;
+  selected: boolean;
 }
 export const MenLink = (props: MenLinkProps): ReactElement => {
   return (
-    <Link className="men-link men-button" to={props.href}>
+    <Link className={`men-link men-button ${props.selected ? "active" : ""}`} to={props.href}>
       {props.label}
     </Link>
   );
