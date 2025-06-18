52bdc479d9bd40cb9f609fabfd54625c5892eb8e
BunniesRabbits <212995513+BunniesRabbits@users.noreply.github.com>
2025-06-17 18:07:08 -0700

    player-panel: generate random colors for helm icons

--- site/src/components/player-panel/player-panel.tsx
+++ site/src/components/player-panel/player-panel.tsx
@@ -4,6 +4,27 @@ import "./player-panel.css";
 import type { NPCInteraction, Stats } from "../../data/api";
 
 /**
+ * cyrb53 (c) 2018 bryc (github.com/bryc)
+ * License: Public domain (or MIT if needed). Attribution appreciated.
+ * A fast and simple 53-bit string hash function with decent collision resistance.
+ * Largely inspired by MurmurHash2/3, but with a focus on speed/simplicity.
+ */
+const cyrb53 = (str: string, seed = 0): number => {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
+/**
  * Time in milliseconds before a player is considered offline/inactive.
  * When that is, they are displayed as all grey.
  */
@@ -81,7 +102,7 @@ const PlayerStats = ({
   lastUpdated?: Date;
   interacting?: NPCInteraction;
 }): ReactElement => {
-  const hueDegrees = 75;
+  const hueDegrees = cyrb53(name) % 360;
 
   const now = new Date();
   const online = now.getTime() - (lastUpdated ?? new Date(0)).getTime() < INACTIVE_TIMER_MS;
