b30493fd135f86d155bf4c8030d4586e40a8cbf5
BunniesRabbits <212995513+BunniesRabbits@users.noreply.github.com>
2025-06-19 16:09:23 -0700

    inventory tooltip done with portals instead of raw html

--- site/src/components/player-panel/player-inventory.tsx
+++ site/src/components/player-panel/player-inventory.tsx
@@ -1,52 +1,33 @@
-import { useRef, type ReactElement } from "react";
+import { useCallback, useRef, useState, type ReactElement } from "react";
 import type { Inventory } from "../../data/api";
 
 import "./player-inventory.css";
 import type { ItemData } from "../../data/item-data";
+import { createPortal } from "react-dom";
 
 export const PlayerInventory = ({ items, itemData }: { items?: Inventory; itemData?: ItemData }): ReactElement => {
-  const tooltip = useRef<HTMLElement>(document.querySelector("#tooltip"));
+  const [selectedItem, setSelectedItem] = useState<{ name: string; quantity: number; highalch: number }>();
+  const tooltipRef = useRef<HTMLDivElement>(document.body.querySelector<HTMLDivElement>("div#tooltip")!);
+
+  const hideTooltip = useCallback(() => {
+    setSelectedItem(undefined);
+    tooltipRef.current.style.visibility = "hidden";
+  }, []);
 
   return (
     <div className="player-inventory">
-      <div
-        onPointerLeave={() => {
-          if (!tooltip.current) return;
-          tooltip.current.innerHTML = "";
-          tooltip.current.style.visibility = "hidden";
-        }}
-        className="player-inventory-background"
-      >
+      <div onPointerLeave={hideTooltip} className="player-inventory-background">
         {(items ?? Array<undefined>(28).fill(undefined)).map((item, index) => {
           if (item === undefined)
-            return (
-              <span
-                onPointerEnter={() => {
-                  if (!tooltip.current) return;
-                  tooltip.current.style.visibility = "hidden";
-                }}
-                className="player-inventory-item-box"
-                key={index}
-              />
-            );
+            return <span onPointerEnter={hideTooltip} className="player-inventory-item-box" key={index} />;
           return (
             <img
               onPointerEnter={() => {
-                if (!tooltip.current) return;
                 const itemDatum = itemData?.get(item.itemID);
                 if (!itemDatum) return;
 
-                tooltip.current.style.visibility = "visible";
-
-                if (item.quantity > 1) {
-                  tooltip.current.innerHTML = `${itemDatum.name} x ${item.quantity}`;
-                } else {
-                  tooltip.current.innerHTML = `${itemDatum.name}`;
-                }
-
-                if (itemDatum.highalch > 0) {
-                  tooltip.current.innerHTML += `<br/>HA: ${itemDatum.highalch}`;
-                }
+                setSelectedItem({ name: itemDatum.name, quantity: item.quantity, highalch: itemDatum.highalch });
+                tooltipRef.current.style.visibility = "visible";
               }}
               key={`${item.itemID} ${item.quantity} ${index}`}
               alt="osrs item"
@@ -56,6 +37,18 @@ export const PlayerInventory = ({ items, itemData }: { items?: Inventory; itemDa
           );
         })}
       </div>
+      {createPortal(
+        selectedItem ? (
+          <div>
+            {selectedItem?.name ?? ""} x {selectedItem?.quantity}
+            <br />
+            HA: {selectedItem?.highalch}
+          </div>
+        ) : (
+          <></>
+        ),
+        tooltipRef.current,
+      )}
     </div>
   );
 };
